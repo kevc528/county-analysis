6524ef8706fd3900f462fca78b7b4e45e571f461
Kevin Chen <kevc528@seas.upenn.edu>
2021-12-18 14:13:40 -0600

    finished download

--- code/0-download.R
+++ code/0-download.R
@@ -1,12 +1,9 @@
 # load libraries
 library(tidyverse)
 
-# download case data from NY Times website
-url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
-case_data_raw = read_csv(url)
+# download raw, unclean county data from American Community Survey
+url = "https://www.openintro.org/data/csv/county_2019.csv"
+counties_raw = read_csv(url)
 
 # write raw data to file
-write_tsv(x = case_data_raw, file = "data/raw/case_data_raw.tsv")
-
-# download health rankings data from the web 
-# (omitted from this template)
+write_csv(x = counties_raw, file = "data/raw/counties_raw.csv")
