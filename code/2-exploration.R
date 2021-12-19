b7ebb20be5cef203ee8e80392566090b614506ff
Kevin Chen <kevc528@seas.upenn.edu>
2021-12-18 19:50:17 -0600

    rename in progress

--- code/2-exploration.R
+++ code/2-exploration.R
@@ -1,41 +1,74 @@
 # load libraries
 library(kableExtra)                     # for printing tables
-library(cowplot)                        # for side by side plots
-library(lubridate)                      # for dealing with dates
 library(maps)                           # for creating maps
 library(tidyverse)
 
+# install.packages("magick")
+# install.packages("webshot")
+# webshot::install_phantomjs()
+
 # read in the cleaned data
-covid_data = read_tsv("data/clean/covid_data.tsv")
+counties_train = read_csv("data/clean/counties_clean.csv")
 
-# calculate median case fatality rate
-median_case_fatality_rate = covid_data %>%
-  summarise(median(case_fatality_rate)) %>%
-  pull()
+# create table with different percentiles of median household income
+counties_train %>% 
+  summarise(
+    "Percentile" = c(0, 25, 50, 75, 100),
+    `Median Household Income` = quantile(median_household_income, c(0, 0.25, 0.5, 0.75, 1))
+  ) %>%
+  kable(format = 'latex', row.names = NA,
+        booktabs = TRUE, digits = 2,
+        caption = 'Percentiles for Median Household Income') %>%
+  kable_styling() %>%
+  save_kable(file ="results/median-household-income-quartiles.png")
 
-# create histogram of case fatality rate
-p = covid_data %>%
-  ggplot(aes(x = case_fatality_rate)) + 
-  geom_histogram() +
-  geom_vline(xintercept = median_case_fatality_rate,
-             linetype = "dashed") +
-  labs(x = "Case fatality rate (percent)", 
-       y = "Number of counties") +
-  theme_bw()
+# create boxplot of the median household income
+(counties_train %>%
+    ggplot(aes(x = median_household_income / 1000)) + 
+    geom_boxplot() +
+    scale_x_continuous(breaks=seq(0,150,10)) +
+    labs(x = "Median Household Income (Thousands of Dollars)", 
+         y = "Number of Counties") +
+    theme_bw()) %>%
+    ggsave(filename = "results/median-household-income-boxplot.png", 
+           device = "png", 
+           width = 5, 
+           height = 3)
 
-# save the histogram
-ggsave(filename = "results/response-histogram.png", 
-       plot = p, 
+# create histogram of the median household income
+(counties_train %>%
+  ggplot(aes(x = median_household_income / 1000)) + 
+  geom_histogram(fill = 'blue', col = 'black') +
+  scale_x_continuous(breaks=seq(0,150,10)) +
+  labs(x = "Median Household Income (Thousands of Dollars)", 
+       y = "Number of Counties") +
+  theme_bw()) %>%
+  ggsave(filename = "results/median-household-income-histogram.png", 
        device = "png", 
        width = 5, 
        height = 3)
 
-# examine top 10 counties by case fatality rate
-covid_data %>% 
-  select(county, state, case_fatality_rate) %>%
-  arrange(desc(case_fatality_rate)) %>%
+# find top 10 counties by median household income
+counties_train %>% 
+  select(name, state, median_household_income) %>%
+  arrange(desc(median_household_income)) %>%
   head(10) %>%
-  write_tsv("results/top-10-counties-data.tsv")
+  kable(format = 'latex', row.names = NA,
+        booktabs = TRUE, digits = 2,
+        caption = 'Top 10 Counties for Median Household Income') %>%
+  kable_styling() %>%
+  save_kable(file ="results/top-10-counties.png")
+
+# find bottom 10 counties by median household income
+counties_train %>% 
+  select(name, state, median_household_income) %>%
+  arrange(median_household_income) %>%
+  head(10) %>%
+  kable(format = 'latex', row.names = NA,
+        booktabs = TRUE, digits = 2,
+        caption = 'Bottom 10 Counties for Median Household Income') %>%
+  kable_styling() %>%
+  save_kable(file ="results/bottom-10-counties.png")
 
 # create a heatmap of case fatality rate across the U.S.
 p = map_data("county") %>%
