521ba2526390175eb0748526cbae663eed6133d1
Arnold Jia <jia.arnold1@gmail.com>
2021-12-19 01:51:26 -0500

    Finished regression modeling

--- code/2-train-test-split.R
+++ code/2-train-test-split.R
@@ -7,11 +7,23 @@ counties_clean = read_csv("data/clean/counties_clean.csv")
 # split into train and test
 set.seed(471)
 
-set.seed(471)
+# generate 80-20 split for train-test split
 train_samples = sample(1:nrow(counties_clean), 0.8*nrow(counties_clean))
-counties_train = counties_clean %>% filter(row_number() %in% train_samples)
-counties_test = counties_clean %>% filter(!(row_number() %in% train_samples))
+
+# see which column names contain the word "income"
+colnames(counties_clean[grepl('income', colnames(counties_clean))])
+
+# generate split and remove correlated income features to keep only `median_household_income`
+counties_train = counties_clean %>%
+  filter(row_number() %in% train_samples) %>%
+  select(-mean_household_income, -median_individual_income, -median_individual_income_age_25plus, -per_capita_income)
+
+counties_test = counties_clean %>% 
+  filter(!(row_number() %in% train_samples)) %>%
+  select(-mean_household_income, -median_individual_income, -median_individual_income_age_25plus, -per_capita_income)
 
 # save the train and test data
 write_csv(x = counties_train, file = "data/clean/counties_train.csv")
 write_csv(x = counties_test, file = "data/clean/counties_test.csv")
+
+
--- code/4-regression-modeling.R
+++ code/4-regression-modeling.R
@@ -1,50 +1,118 @@
 # load libraries
+library(tidyverse)
 library(glmnetUtils)                    # to run ridge and lasso
-source("code/functions/plot_glmnet.R")            # for lasso/ridge trace plots
+source("code/functions/plot_glmnet.R")  # for lasso/ridge trace plots
 
 # read in the training data
-covid_train = read_tsv("data/clean/covid_train.tsv")
+counties_train = read_csv("data/clean/counties_train.csv")
+set.seed(471)
 
-# run ridge regression
-set.seed(1)
-ridge_fit = cv.glmnet(case_fatality_rate ~ . - state - county - fips,   
-                      alpha = 0,                 
-                      nfolds = 10,               
-                      data = covid_train)
+# =========================RUN OLS REGRESSION==========================
+lm_fit = lm(formula = median_household_income ~ . -state -name -fips, 
+            data = counties_train)
+
+
+# =========================RUN RIDGE REGRESSION=========================
+set.seed(471)
+ridge_fit = cv.glmnet(median_household_income ~ . -state -name -fips, 
+                      alpha = 0, 
+                      nfolds = 10,
+                      data = counties_train)
 
 # save the ridge fit object
-save(ridge_fit, file = "results/ridge_fit.Rda")
+save(ridge_fit, file = "results/model-results/ridge_fit.Rda")
 
-# run lasso regression
-set.seed(1)
-lasso_fit = cv.glmnet(case_fatality_rate ~ . - state - county - fips,   
+# create ridge CV plot
+png(width = 6, 
+    height = 4,
+    res = 300,
+    units = "in", 
+    filename = "results/model-results/ridge-cv-plot.png")
+plot(ridge_fit)
+dev.off()
+
+# create ridge trace plot
+ridge_plot = plot_glmnet(ridge_fit, counties_train, features_to_plot = 6)
+ggsave(filename = "results/model-results/ridge-trace-plot.png", 
+       plot = ridge_plot, 
+       device = "png", 
+       width = 6.5, 
+       height = 4)
+
+
+# =========================RUN LASSO REGRESSION==========================
+set.seed(471)
+lasso_fit = cv.glmnet(median_household_income ~ . -state -name -fips,   
                       alpha = 1,                 
                       nfolds = 10,               
-                      data = covid_train)
+                      data = counties_train)
 
 # save the lasso fit object
-save(lasso_fit, file = "results/lasso_fit.Rda")
+save(lasso_fit, file = "results/model-results/lasso_fit.Rda")
 
 # create lasso CV plot
 png(width = 6, 
     height = 4,
     res = 300,
     units = "in", 
-    filename = "results/lasso-cv-plot.png")
+    filename = "results/model-results/lasso-cv-plot.png")
 plot(lasso_fit)
 dev.off()
 
 # create lasso trace plot
-p = plot_glmnet(lasso_fit, covid_train, features_to_plot = 6)
-ggsave(filename = "results/lasso-trace-plot.png", 
-       plot = p, 
+lasso_plot = plot_glmnet(lasso_fit, counties_train, features_to_plot = 6)
+ggsave(filename = "results/model-results/lasso-trace-plot.png", 
+       plot = lasso_plot, 
        device = "png", 
-       width = 6, 
+       width = 6.5, 
        height = 4)
 
 # extract features selected by lasso and their coefficients
-beta_hat_std = extract_std_coefs(lasso_fit, covid_train)
+beta_hat_std = extract_std_coefs(lasso_fit, counties_train)
 beta_hat_std %>%
   filter(coefficient != 0) %>%
   arrange(desc(abs(coefficient))) %>% 
-  write_tsv("results/lasso-features-table.tsv")
+  write_csv("results/model-results/lasso-features-table.csv")
+
+
+# =========================RUN ELASTIC NET REGRESSION=======================
+set.seed(471)
+elnet_fit = cva.glmnet(median_household_income ~ . -state -name -fips,   
+                      nfolds = 10,               
+                      data = counties_train)
+
+elnet_fit$alpha
+
+# save the elastic net fit object
+save(elnet_fit, file = "results/model-results/elnet_fit.Rda")
+
+# create elastic net CV plot for alpha
+png(width = 6, 
+    height = 4,
+    res = 300,
+    units = "in", 
+    filename = "results/model-results/elnet-cv-alpha-plot.png")
+plot_cva_glmnet(elnet_fit)
+dev.off()
+
+# extract optimal elastic net
+elnet_fit_best = extract_best_elnet(elnet_fit)
+
+# create elastic net CV plot with optimal alpha
+png(width = 6, 
+    height = 4,
+    res = 300,
+    units = "in", 
+    filename = "results/model-results/elnet-cv-plot.png")
+plot(elnet_fit_best)
+dev.off()
+
+# create elastic net trace plot
+elnet_plot = plot_glmnet(elnet_fit_best, counties_train, features_to_plot = 6)
+ggsave(filename = "results/model-results/elnet-trace-plot.png", 
+       plot = elnet_plot, 
+       device = "png", 
+       width = 6.5, 
+       height = 4)
+
+
